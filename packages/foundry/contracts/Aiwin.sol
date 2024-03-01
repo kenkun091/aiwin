0ce95bf7ec30004202b0d4171bdf74ef83253157
RileyDesrochers <rileydesrochers@gmail.com>
2024-03-01 16:39:28 -0700

    imported all the libs

---
+++ packages/foundry/contracts/Aiwin.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.23;
+
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract GameItem is ERC721URIStorage {
+    uint256 private _nextTokenId;
+
+    constructor() ERC721("GameItem", "ITM") {}
+
+    function awardItem(address player, string memory tokenURI) public returns (uint256) {
+        uint256 tokenId = _nextTokenId++;
+        _mint(player, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+
+        return tokenId;
+    }
+}
--- packages/foundry/contracts/interfaces/resolvers/IResolver.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.23;
-
-/// @notice Resolver Interface
-interface IResolver {
-    /// @notice Checks whether the resolver IP interface is supported.
-    function supportsInterface(bytes4 id) external view returns (bool);
-}
